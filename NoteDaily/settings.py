e3f261a109affe2960028669042b665578b09d31
yishierniao36 <1208155441@qq.com>
2015-07-15 20:48:32 +0800

    修改了setings.py配置文件，添加style.css、images

--- NoteDaily/settings.py
+++ NoteDaily/settings.py
@@ -103,4 +103,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' 
+
+
+STATICFILES_DIRS = (
+    ("css", os.path.join(BASE_DIR,'static/css')),
+    ("js", os.path.join(BASE_DIR,'static/js')),
+    ("images", os.path.join(BASE_DIR,'static/images')),
+) 
+
