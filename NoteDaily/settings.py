815ad373eed45ab2906f1512cab5267549ec25bd
kanew3z2m2 <821118798@qq.com>
2015-07-31 20:25:39 +0800

    添加STATIC_ROOT

--- NoteDaily/settings.py
+++ NoteDaily/settings.py
@@ -103,9 +103,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+STATIC_ROOT = '/home/wzm/static'
 
 STATIC_URL = '/static/' 
-
 
 STATICFILES_DIRS = (
     ("css", os.path.join(BASE_DIR,'static/css')),
